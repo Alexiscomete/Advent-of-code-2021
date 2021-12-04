01855d91df61488c0e37577b93ae7acc08b90ae5
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-04 12:07:34 +0100

    day4 part 1 (suite)

--- src/src/main/days/day4/BingoGrid.java
+++ src/src/main/days/day4/BingoGrid.java
@@ -1,11 +1,26 @@
 package main.days.day4;
 
-public class BingoGrid {
+public record BingoGrid(BingoCase[][] bingoCases) {
 
-    BingoCase[][] bingoCases;
+    public void setMarkCase(int i, int j, boolean value) {
+        bingoCases[i][j].setMarked(value);
+    }
 
-    public BingoGrid(BingoCase[][] bingoCases) {
-        this.bingoCases = bingoCases;
+    public boolean victory() {
+
+        for (BingoCase[] bingoCase : bingoCases) {
+            if (bingoCase[0].isMarked() && bingoCase[1].isMarked() && bingoCase[2].isMarked() && bingoCase[3].isMarked() && bingoCase[4].isMarked()) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (bingoCases[0][i].isMarked() && bingoCases[1][i].isMarked() && bingoCases[2][i].isMarked() && bingoCases[3][i].isMarked() && bingoCases[4][i].isMarked()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
