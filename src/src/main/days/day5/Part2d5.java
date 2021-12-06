7a5befb7528484f486604147a6365c6991990ccd
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-06 12:17:17 +0100

    rien

--- src/src/main/days/day5/Part2d5.java
+++ src/src/main/days/day5/Part2d5.java
@@ -8,7 +8,6 @@ public class Part2d5 extends Part {
 
     public Part2d5() {
         final HashMap<Integer, HashMap<Integer, Short>> map = new HashMap<>();
-        final int[] num = {0};
 
         getInput.forEach((str) -> {
             System.out.println(str);
@@ -42,30 +41,22 @@ public class Part2d5 extends Part {
                     for (int j = ymin; j < ymax + 1; j++) {
                         if (!tempMap.containsKey(j)) {
                             tempMap.put(j, (short) 1);
-                        } else if (tempMap.get(j) == 1){
-                            System.out.println("+1hv");
-                            tempMap.replace(j, (short) (2));
-                            num[0]++;
+                        } else {
+                            System.out.println(tempMap.get(j) + 1);
+                            tempMap.replace(j, (short) (tempMap.get(j) + 1));
                         }
                     }
                 }
             } else {
-                int xmin, xmax, ymin, ymax;
-                if (x1 < x2) {
-                    xmin = x1;
-                    xmax = x2;
-                } else {
-                    xmin = x2;
-                    xmax = x1;
+                int plusY = 1;
+                if (y1 > y2) {
+                    plusY = -1;
                 }
-                if (y1 < y2) {
-                    ymin = y1;
-                    ymax = y2;
-                } else {
-                    ymin = y2;
-                    ymax = y1;
+                int plusX = 1;
+                if (x1 > x2) {
+                    plusX = -1;
                 }
-                for (int i = xmin, j = ymin; i < xmax +1 && j < ymax +1; i++, j++) {
+                for (int i = x1, j = y1; i != x2 && j != y2; i += plusX, j += plusY) {
                     HashMap<Integer, Short> tempMap;
                     if (!map.containsKey(i)) {
                         tempMap = new HashMap<>();
@@ -76,15 +67,24 @@ public class Part2d5 extends Part {
 
                     if (!tempMap.containsKey(j)) {
                         tempMap.put(j, (short) 1);
-                    } else if (tempMap.get(j) == 1){
-                        System.out.println("+1d");
-                        tempMap.replace(j, (short) (2));
-                        num[0]++;
+                    } else {
+                        System.out.println(tempMap.get(j) + 1);
+                        tempMap.replace(j, (short) (tempMap.get(j) + 1));
                     }
                 }
             }
         });
 
-        System.out.println(num[0]);
+        int total = 0;
+
+        for (HashMap<Integer, Short> tpmp : map.values()) {
+            for (short sh : tpmp.values()) {
+                if (sh > 1) {
+                    total++;
+                }
+            }
+        }
+
+        System.out.println(total);
     }
 }
