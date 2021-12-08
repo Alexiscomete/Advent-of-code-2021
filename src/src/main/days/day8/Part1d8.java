3628ed9322c70d8dcd7c016fdf61c7a2c6f7c2a5
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-08 18:21:25 +0100

    début d8

--- src/src/main/days/day8/Part1d8.java
+++ src/src/main/days/day8/Part1d8.java
@@ -3,4 +3,18 @@ package main.days.day8;
 import main.Part;
 
 public class Part1d8 extends Part {
+    public Part1d8() {
+        int[] i = {0};
+
+        getInput.forEach(s -> {
+            String[] strings = s.substring(s.indexOf("|") + 1).split(" ");
+            for (String digit : strings) {
+                if (digit.length() == 3 || digit.length() == 4 || digit.length() == 2 || digit.length() == 7) {
+                    i[0]++;
+                }
+            }
+        });
+
+        System.out.println(i[0]);
+    }
 }
--- src/src/main/days/day8/Part2d8.java
+++ src/src/main/days/day8/Part2d8.java
@@ -2,5 +2,37 @@ package main.days.day8;
 
 import main.Part;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Part2d8 extends Part {
+    public Part2d8() {
+        int[] i = {0};
+        final Character[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+        getInput.forEach(s -> {
+            String[] strings = s.substring(s.indexOf("|") + 1).split(" ");
+            List<Character> charactersH0 = new ArrayList<>(), charactersH1 = new ArrayList<>(), charactersH2 = new ArrayList<>(), charactersV0R = new ArrayList<>(), charactersV0L = new ArrayList<>(), charactersV1R = new ArrayList<>(), charactersV1L = new ArrayList<>();
+            Collections.addAll(charactersH0, chars);
+            Collections.addAll(charactersH1, chars);
+            Collections.addAll(charactersH2, chars);
+            Collections.addAll(charactersV0L, chars);
+            Collections.addAll(charactersV0R, chars);
+            Collections.addAll(charactersV1L, chars);
+            Collections.addAll(charactersV1R, chars);
+
+            for (String digit : strings) {
+                if (digit.length() == 2) {
+                    for (Character c : charactersH0) {
+                        if (!digit.contains(c)) {
+
+                        }
+                    }
+                }
+            }
+        });
+
+        System.out.println(i[0]);
+    }
 }
