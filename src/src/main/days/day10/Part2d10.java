b7ef8e92aae601c979ecfc735bbb616a8a5de652
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-11 10:49:30 +0100

    d10 fin

--- src/src/main/days/day10/Part2d10.java
+++ src/src/main/days/day10/Part2d10.java
@@ -2,56 +2,72 @@ package main.days.day10;
 
 import main.Part;
 
+import java.util.ArrayList;
+
 public class Part2d10 extends Part {
     StringBuilder str = new StringBuilder();
 
     public Part2d10() {
-        int[] errors = {0};
+        ArrayList<Long> integers = new ArrayList<>();
         getInput.forEach(s -> {
             str = new StringBuilder();
-            errors[0] += errorDetect(s);
+            if (!errorDetect(s) && str.length() > 0) {
+                long score = 0;
+                for (int i = str.length()-1; i >= 0; i--) {
+                    char c = str.charAt(i);
+                    score *= 5;
+                    switch (c) {
+                        case '(' -> score += 1;
+                        case '{' -> score += 3;
+                        case '[' -> score += 2;
+                        case '<' -> score += 4;
+                    }
+                }
+                integers.add(score);
+            }
         });
-
-        System.out.println(errors[0]);
+        integers.sort(Long::compareTo);
+        System.out.println(integers);
+        System.out.println(integers.get((int) (integers.size()/2+0.5)));
     }
 
-    public int errorDetect(String s) {
+    public boolean errorDetect(String s) {
         for (char character : s.toCharArray()) {
             if (character == '{' || character == '[' || character == '(' || character == '<') {
                 str.append(character);
             } else {
-                switch (character){
+                switch (character) {
                     case '}':
-                        if (str.charAt(str.length()-1) == '{') {
-                            str.deleteCharAt(str.length()-1);
+                        if (str.charAt(str.length() - 1) == '{') {
+                            str.deleteCharAt(str.length() - 1);
                         } else {
-                            return 1197;
+                            return true;
                         }
                         break;
                     case ']':
-                        if (str.charAt(str.length()-1) == '[') {
-                            str.deleteCharAt(str.length()-1);
+                        if (str.charAt(str.length() - 1) == '[') {
+                            str.deleteCharAt(str.length() - 1);
                         } else {
-                            return 57;
+                            return true;
                         }
                         break;
                     case ')':
-                        if (str.charAt(str.length()-1) == '(') {
-                            str.deleteCharAt(str.length()-1);
+                        if (str.charAt(str.length() - 1) == '(') {
+                            str.deleteCharAt(str.length() - 1);
                         } else {
-                            return 3;
+                            return true;
                         }
                         break;
                     case '>':
-                        if (str.charAt(str.length()-1) == '<') {
-                            str.deleteCharAt(str.length()-1);
+                        if (str.charAt(str.length() - 1) == '<') {
+                            str.deleteCharAt(str.length() - 1);
                         } else {
-                            return 25137;
+                            return true;
                         }
                         break;
                 }
             }
         }
-        return 0;
+        return false;
     }
 }
