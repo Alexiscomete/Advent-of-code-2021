97e6bfd8739b32d3ad14a062e1b6aa126c415323
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-12 13:10:10 +0100

    d12 suite

--- src/src/main/days/day12/EndCave.java
+++ src/src/main/days/day12/EndCave.java
@@ -2,6 +2,8 @@ package main.days.day12;
 
 public class EndCave extends Cave {
 
+    public static int paths = 0;
+
     public EndCave() {
         super("end");
     }
--- src/src/main/days/day12/Part1d12.java
+++ src/src/main/days/day12/Part1d12.java
@@ -1,4 +1,9 @@
 package main.days.day12;
 
-public class Part1d12 {
+public class Part1d12 extends PartD12 {
+    public Part1d12() {
+        getInput.forEach(this::addPath);
+        getCaveByName("start").findPath();
+        System.out.println(EndCave.paths);
+    }
 }
