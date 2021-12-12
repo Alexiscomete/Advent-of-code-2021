7b0562dc534ef7358e0a31ab7d5a5343d5d64764
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-12 14:04:46 +0100

    d12 fin

--- src/src/main/days/day12/SmallCave.java
+++ src/src/main/days/day12/SmallCave.java
@@ -3,6 +3,8 @@ package main.days.day12;
 public class SmallCave extends Cave {
 
     private boolean canTravel = true;
+    private static boolean twice = true;
+    private boolean twiceInstance = false;
 
     public SmallCave(String name) {
         super(name);
@@ -10,18 +12,33 @@ public class SmallCave extends Cave {
 
     @Override
     void findPath(PartD12 partD12) {
-        canTravel = false;
+        if (!twiceInstance) canTravel = false;
         for (String str : paths) {
             Cave cave = partD12.getCaveByName(str);
             if (cave.canTravelTo()) {
                 cave.findPath(partD12);
             }
         }
-        canTravel = true;
+        if (twiceInstance) {
+            twiceInstance = false;
+            twice = true;
+        } else {
+            canTravel = true;
+        }
     }
 
     @Override
     public boolean canTravelTo() {
-        return canTravel;
+        if (canTravel) {
+            return true;
+        } else {
+            if (twice) {
+                twice = false;
+                twiceInstance = true;
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
